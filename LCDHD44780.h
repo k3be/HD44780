13f0d93ee2941791c3a13a8f11696ac6dcda4aa9
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-09-18 20:50:19 +0200

    Update LCDHD44780.h

--- LCDHD44780.h
+++ LCDHD44780.h
@@ -52,6 +52,7 @@
 
 // Define GPIO pin to LCD pin mapping
 #define LCD_RS RPI_V2_GPIO_P1_26
+#define LCD_RW RPI_V2_GPIO_P1_07
 #define LCD_E  RPI_V2_GPIO_P1_24
 #define LCD_D4 RPI_V2_GPIO_P1_22 
 #define LCD_D5 RPI_V2_GPIO_P1_18
